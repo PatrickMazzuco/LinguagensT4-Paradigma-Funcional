70b2186d7baba819aec929ac048cb9685e78ddbf
EDUARDO SCHWEITZER NUNES DA SILVA <eduardo.schweitzer@acad.pucrs.br>
2018-10-25 22:04:12 -0300

    Adicionado a versão base do métdodo para escrever arquivos

--- App.ml
+++ App.ml
@@ -1,1 +1,8 @@
 (*Versão 0.0.1*)
+
+fun writePoem(filename) =
+    let val file = TextIO.openOut(filename)
+        val _ = TextIO.output(file, "Roses are red,\nViolets are blue.\n")
+        val _ = TextIO.output(file, "I have a gun.\nGet in the van.\n")
+    in TextIO.closeOut(file)
+    end
